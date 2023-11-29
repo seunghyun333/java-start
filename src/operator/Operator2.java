8c9e852622b8f5aec851a2563fa55b84a534330c
seunghyun333 <shb2in33@naver.com>
2023-11-29 23:03:49 +0900

    연산자 1,2

---
+++ src/operator/Operator2.java
@@ -0,0 +1,23 @@
+package operator;
+
+public class Operator2 {
+    public static void main(String[] args) {
+        String result1 = "hello" + "world";
+        System.out.println(result1);
+
+        String s1 = "string1";
+        String s2 = "string2";
+        String result2 = s1 + s2;
+        System.out.println(result2);
+
+        String result3 = "a + b = " + 10;
+        System.out.println(result3);
+
+        int num = 20;
+        String str = "a + b = ";
+        String result4 = str + num;
+        System.out.println(result4);
+
+        //자바는 문자열인 `String` 타입에 다른 타입을 더하는 경우 대상 타입을 문자열로 변경한다
+    }
+}
