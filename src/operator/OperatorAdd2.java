157c9a26297f86559cd0e090fa6588972c8e9671
seunghyun333 <shb2in33@naver.com>
2023-11-30 23:43:01 +0900

    3.연산자 2

---
+++ src/operator/OperatorAdd2.java
@@ -0,0 +1,19 @@
+package operator;
+
+public class OperatorAdd2 {
+    public static void main(String[] args) {
+        // 전위 증감 연산자
+        int a = 1;
+        int b = 0;
+        b = ++a;
+        System.out.println("a = " + a + ", b = " + b);
+        //a = 2, b = 2
+
+        // 후위 증감 연산자
+        a = 1;
+        b = 0;
+        b = a++;
+        System.out.println("a = " + a + ", b = " + b);
+        //a = 2, b = 1
+    }
+}
