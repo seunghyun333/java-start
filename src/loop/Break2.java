1aae7a014d13af3349f03f4812e92588d327ffa6
seunghyun333 <shb2in33@naver.com>
2023-12-03 21:19:33 +0900

    for 문

---
+++ src/loop/Break2.java
@@ -0,0 +1,17 @@
+package loop;
+
+public class Break2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 1;
+
+        for (; ;) { // 조건이 없으면 무한 반복
+            sum += i;
+            if (sum > 10) {
+                System.out.println("합이 10보다 크면 종료: i = " + i + " sum = " + sum);
+                break;
+            }
+            i++;
+        }
+    }
+}
